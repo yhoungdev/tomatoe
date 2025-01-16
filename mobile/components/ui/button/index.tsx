275dde3fee377fcf9a2747c908b0ec71d8356214
obiabo <54102389+yhoungdev@users.noreply.github.com>
2025-01-16 16:37:47 +0100

    📺 updated mobile intro page

---
+++ mobile/components/ui/button/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import styles from './button'
+
+interface  IButtonProps {
+    title: string;
+    onPress: () => void;
+}
+
+const PrimaryButton = ({ title, onPress }: IButtonProps) => (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+);
+
+export default PrimaryButton;
---
+++ mobile/layouts/containerLayout.tsx
@@ -0,0 +1,21 @@
+import React, {JSX} from 'react';
+import { View , StyleSheet
+} from 'react-native';
+
+const ContainerStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    }
+})
+
+
+function ContainerLayout({children}: {children: JSX.Element}): JSX.Element {
+    return (
+        <View style={ContainerStyle.container}>
+            {children}
+        </View>
+    );
+}
+
+export default ContainerLayout;
